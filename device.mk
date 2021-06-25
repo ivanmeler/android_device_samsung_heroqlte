12f8d88de267a8663a202f6593227ea4ea4cf61a
Deokgyu Yang <secugyu@gmail.com>
2021-06-25 17:49:55 +0900

    gts3llte: ril: Add radio.deprecated
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I88b850664973477226838f0f9762e8a78efb1389

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.1 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
