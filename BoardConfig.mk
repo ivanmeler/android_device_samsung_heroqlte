a982a9718b2e5c83c5d0d8d8f8a937adad492b19
Deokgyu Yang <secugyu@gmail.com>
2021-12-04 13:50:58 +0900

    gts3llte: boardconfig: Change the target defconfig file name
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I805e03d902085b5530dc4684e265fb889a408031

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineage_gts3llte_defconfig
+TARGET_KERNEL_CONFIG := gts3llte_defconfig
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
